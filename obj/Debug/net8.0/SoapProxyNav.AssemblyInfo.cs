c6241c99b73f1f43c11ea23608e69dccdb9847b0
rdmasquesalud <rdmasquesalud@gmail.com>
2025-05-25 20:07:09 -0400

    segunda

--- obj/Debug/net8.0/SoapProxyNav.AssemblyInfo.cs
+++ obj/Debug/net8.0/SoapProxyNav.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SoapProxyNav")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f186bb30c6ee89b001e3c94a753fcffbbc71c610")]
 [assembly: System.Reflection.AssemblyProductAttribute("SoapProxyNav")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SoapProxyNav")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
